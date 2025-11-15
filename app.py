18651a8decb90da521ce85781642778810da3b2b
Ausake <kevin.lefebvre.tech@gmail.com>
2025-11-15 17:15:15 +0100

    Add input check

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 
 from utils import clean_text, count_frequency, sort_by_frequency_desc, export_frequencies_to_csv
+from pathlib import Path
+
+
+INPUT_FILE = Path("input.txt")
+
 
 def main():
+    if not INPUT_FILE.exists():
+        raise FileNotFoundError(f"Input file not found: {INPUT_FILE}")
+
     with open("input.txt", "r", encoding="utf-8") as file:
         content = file.read()
 
